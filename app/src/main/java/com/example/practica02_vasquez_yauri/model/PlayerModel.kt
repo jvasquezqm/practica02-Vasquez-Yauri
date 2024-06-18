613baa0b3fa9dae9f2099026ac3d6fed7476901a
Jonathan <jvasquezqm@gmail.com>
2024-06-18 00:06:48 -0500

    creación de adapter

---
+++ app/src/main/java/com/example/practica02_vasquez_yauri/model/PlayerModel.kt
@@ -0,0 +1,8 @@
+package com.example.practica02_vasquez_yauri.model
+
+data class PlayerModel(
+    val name: String="",
+    val team: String="",
+    val position: String="",
+    val image: String=""
+)
